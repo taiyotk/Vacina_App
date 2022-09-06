89833af2970ffd6bca10452878434da49ac51b05
gabrielsanic <ghenrique018@gmail.com>
2022-09-06 00:20:48 -0300

    home layout

--- app/src/main/java/com/example/vacinaapp/MainActivity.kt
+++ app/src/main/java/com/example/vacinaapp/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.example.vacinaapp
 
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.MenuItem
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.content.OnConfigurationChangedProvider
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.example.vacinaapp.fragments.*
-import com.google.android.material.navigation.NavigationView
+import com.example.vacinaapp.fragments.InicioFragment
+import com.example.vacinaapp.fragments.LocaisFragment
+import com.example.vacinaapp.fragments.PesquisarFragment
+import com.example.vacinaapp.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-
-
-class MainActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-    private lateinit var drawer: DrawerLayout
-    private lateinit var toggle: ActionBarDrawerToggle
-
+class MainActivity : AppCompatActivity() {
 
     private val inicioFragment = InicioFragment()
     private val locaisFragment = LocaisFragment()
-    private val configFragment = ConfigFragment()
+    private val settingsFragment = SettingsFragment()
     private val pesquisarFragment = PesquisarFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,50 +21,23 @@ class MainActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelected
         setContentView(R.layout.activity_main)
         replaceFragment(inicioFragment)
 
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
-        setSupportActionBar(toolbar)
 
-        drawer = findViewById(R.id.drawer_layout)
-
-        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close)
-        drawer.addDrawerListener(toggle)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeButtonEnabled(true)
-
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.ic_home -> replaceFragment(inicioFragment)
-            R.id.ic_local -> replaceFragment(locaisFragment)
-            R.id.ic_syringe -> replaceFragment(pesquisarFragment)
-            R.id.ic_config -> replaceFragment(configFragment)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_home -> replaceFragment(inicioFragment)
+                R.id.ic_local -> replaceFragment(locaisFragment)
+                R.id.ic_syringe -> replaceFragment(pesquisarFragment)
+                R.id.ic_config -> replaceFragment(settingsFragment)
+            }
+            true
         }
-        drawer.closeDrawer(GravityCompat.START)
-        return true
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        if(fragment != null) {
+        if (fragment != null) {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container, fragment)
             transaction.commit()
         }
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        toggle.syncState()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
