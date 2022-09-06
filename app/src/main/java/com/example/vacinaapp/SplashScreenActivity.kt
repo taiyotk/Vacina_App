ead47464f23c58703bb0f9f2821d0b3520100ae2
gabrielsanic <ghenrique018@gmail.com>
2022-09-05 23:35:08 -0300

    Home

--- app/src/main/java/com/example/vacinaapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/vacinaapp/SplashScreenActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
-class SplashScreenActivity: AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -14,7 +14,7 @@ class SplashScreenActivity: AppCompatActivity() {
         iv_logo.animate().setDuration(1500).alpha(1f).withEndAction {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
 
             finish()
         }
