ad6c0581c013647b39554bd6ac1b2806b5272ccb
GabrielSanic <ghenrique018@gmail.com>
2022-10-25 09:47:33 -0300

    DialogFragment Publico

---
+++ app/src/main/java/com/example/vacinaapp/fragments/DialogVerFragment.kt
@@ -0,0 +1,22 @@
+package com.example.vacinaapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.vacinaapp.R
+
+class DialogVerFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_dialog_ver, container, false)
+    }
+
+}
+
+
+
--- app/src/main/java/com/example/vacinaapp/recyclerViewAdapters/VacinasAdapter.kt
+++ app/src/main/java/com/example/vacinaapp/recyclerViewAdapters/VacinasAdapter.kt
@@ -1,22 +1,35 @@
 package com.example.vacinaapp.recyclerViewAdapters
 
+import android.app.AlertDialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vacinaapp.R
 import com.example.vacinaapp.dataClass.VacinasDataClass
+import kotlinx.android.synthetic.main.fragment_dialog_ver.view.*
 
 
-class VacinasAdapter(private val listaVacinas: ArrayList<VacinasDataClass>, val clickLambda: (Int)->Unit): RecyclerView.Adapter<VacinasViewHolder>() {
+class VacinasAdapter(
+    private val listaVacinas: ArrayList<VacinasDataClass>,
+    val clickLambda: (Int) -> Unit
+) : RecyclerView.Adapter<VacinasViewHolder>() {
+
+    private lateinit var context: Context
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacinasViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_vacina,parent,false)
+
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_item_vacina, parent, false)
         return VacinasViewHolder(itemView)
+
     }
 
     override fun getItemCount(): Int {
@@ -31,18 +44,28 @@ class VacinasAdapter(private val listaVacinas: ArrayList<VacinasDataClass>, val 
         //holder.ver.text = currentItem.publico
 
         //funcao que muda a cor do sim para verde e não para vermelho
-        if (holder.disponibilidade.text == "Sim"){
+        if (holder.disponibilidade.text == "Sim") {
             holder.disponibilidade.setTextColor(Color.parseColor("#00b400"))
             holder.disponibilidade.setTypeface(null, Typeface.BOLD)
-        } else if(holder.disponibilidade.text == "Não"){
+        } else if (holder.disponibilidade.text == "Não") {
             holder.disponibilidade.setTextColor(Color.parseColor("#db0200"))
             holder.disponibilidade.setTypeface(null, Typeface.BOLD)
         }
 
         //clicklistener do Publico
         holder.ver.setOnClickListener {
+
             clickLambda(currentItem.id_posto)
-            Toast.makeText(holder.itemView.context, "Ver foi Clicado!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(holder.itemView.context, "Ver foi Clicado!", Toast.LENGTH_SHORT).show()
+
+            context = holder.itemView.context
+            val verDialogView = LayoutInflater.from(context).inflate(R.layout.fragment_dialog_ver, null)
+            val builder = AlertDialog.Builder(context).setView(verDialogView)
+
+            verDialogView.ver.text = currentItem.publico
+
+            builder.show()
+
         }
 
         //clicklistener do Editar
@@ -50,16 +73,14 @@ class VacinasAdapter(private val listaVacinas: ArrayList<VacinasDataClass>, val 
             Toast.makeText(holder.itemView.context, "Editar clicado!", Toast.LENGTH_SHORT).show()
         }
     }
-
-
-
 }
 
 class VacinasViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val doenca: TextView = itemView.findViewById(R.id.doenca_textview)
     val disponibilidade: TextView = itemView.findViewById(R.id.disp_textview)
-    val ver: TextView = itemView.findViewById(R.id.ver_textview)
+    val ver: ImageButton = itemView.findViewById(R.id.ver_botao)
     val edit: TextView = itemView.findViewById(R.id.edit_icon_clickable)
 
 }
+
