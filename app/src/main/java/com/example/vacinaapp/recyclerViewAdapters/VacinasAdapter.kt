cc0a73cfaba6d0687eae30dc706f9bae8e146bbb
Taiyo <taiyo@unipam.edu.br>
2022-11-22 11:28:05 -0300

    Adaptação para mudar a visibilidade das edição de vacinas

--- app/src/main/java/com/example/vacinaapp/recyclerViewAdapters/VacinasAdapter.kt
+++ app/src/main/java/com/example/vacinaapp/recyclerViewAdapters/VacinasAdapter.kt
@@ -12,7 +12,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
-
 import androidx.fragment.app.FragmentActivity
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
